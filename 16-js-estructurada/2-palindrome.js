21d300ff1906cd8262f8c45381745d02f2658e41
David Ascencio <david1906@outlook.com>
2022-05-14 22:51:56 -0500

    Feat: Ejercicios de la semana 16

---
+++ 16-js-estructurada/2-palindrome.js
@@ -0,0 +1,31 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+const ORIGIN = 0;
+const TARGET = 1;
+const REPLACEMENTS = [
+  [" ", "á", "é", "í", "ó", "ú", "Á", "É", "Í", "Ó", "Ú", 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  [ "", "a", "e", "i", "o", "u", "a", "e", "i", "o", "u", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+];
+
+const text = console.readString("Escriba un palíndromo: ");
+
+let normalizedText = "";
+for (let i = 0; i < text.length; i++) {
+  let letter = text[i];
+  for (let j = 0; text[i] === letter && j < REPLACEMENTS[ORIGIN].length; j++) {
+    if (text[i] === REPLACEMENTS[ORIGIN][j]) {
+      letter = REPLACEMENTS[TARGET][j];
+    }
+  }
+  normalizedText += letter;
+}
+
+let isPalindrome = true;
+for (let i = 0, j = normalizedText.length - 1; isPalindrome && i < j; i++, j--) {
+  isPalindrome &&= normalizedText[i] === normalizedText[j];
+}
+
+console.writeln(
+  `El texto "${text}" ${isPalindrome ? "sí" : "no"} es palíndromo.`
+);
---
+++ 16-js-estructurada/3-orderedSeries.js
@@ -0,0 +1,17 @@
+const { Console } = require("console-mpds");
+const console = new Console();
+
+const EXIT_NUMBER = 0;
+
+let number;
+let lastnumber;
+let isOrdered = true;
+do {
+  lastnumber = number;
+  number = console.readNumber("Ingresa un número: ");
+  if (number !== EXIT_NUMBER && lastnumber !== undefined) {
+    isOrdered &&= lastnumber < number;
+  }
+} while (number !== EXIT_NUMBER);
+
+console.writeln(`La serie ${isOrdered ? "sí" : "no"} está ordenada`);
